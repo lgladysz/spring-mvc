aa1539a578fecd13a79f68cff9a35bf7a387face
Łukasz Gładysz <dev@gladysz.me>
2017-05-15 19:59:54 +0200

    Add Product Controller

---
+++ src/main/java/me/gladysz/controller/ProductController.java
@@ -0,0 +1,25 @@
+package me.gladysz.controller;
+
+import me.gladysz.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model) {
+
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+}
