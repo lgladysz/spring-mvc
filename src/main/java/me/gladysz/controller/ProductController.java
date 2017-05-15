f0d7106b63aff5f08e712a9eca45bbc55a61c11e
Łukasz Gładysz <dev@gladysz.me>
2017-05-15 23:01:25 +0200

    Add Product details view

--- src/main/java/me/gladysz/controller/ProductController.java
+++ src/main/java/me/gladysz/controller/ProductController.java
@@ -4,6 +4,7 @@ import me.gladysz.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +22,12 @@ public class ProductController {
 
         model.addAttribute("products", productService.listAllProducts());
         return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Long id, Model model) {
+
+        model.addAttribute("product", productService.getProductById(id));
+        return "product";
     }
 }
