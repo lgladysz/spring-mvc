dfa0087a588a23d29877aee4944f4362488109be
Łukasz Gładysz <dev@gladysz.me>
2017-06-01 17:21:16 +0200

    Extract basic CRUD interface

---
+++ src/main/java/me/gladysz/service/CRUDService.java
@@ -0,0 +1,13 @@
+package me.gladysz.service;
+
+import java.util.List;
+
+public interface CRUDService<T> {
+    List<?> listAll();
+
+    T getById(Long id);
+
+    T saveOrUpdate(T domainObject);
+
+    void delete(Long id);
+}
