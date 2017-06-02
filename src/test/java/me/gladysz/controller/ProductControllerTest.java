bfb199387945c6576a85733d50c38e6a1845d42c
Łukasz Gładysz <dev@gladysz.me>
2017-06-02 19:07:47 +0200

    Add tests for product controller

---
+++ src/test/java/me/gladysz/controller/ProductControllerTest.java
@@ -0,0 +1,142 @@
+package me.gladysz.controller;
+
+import me.gladysz.model.Product;
+import me.gladysz.service.ProductService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ProductControllerTest {
+
+    @Mock
+    private ProductService productService;
+
+    @InjectMocks
+    private ProductController productController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
+    }
+
+    @Test
+    public void listProducts() throws Exception {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product());
+        products.add(new Product());
+
+        //specific Mockito interaction, tell stub to return list of products
+        when(productService.listAll()).thenReturn((List) products); //need to strip generics to keep Mockito happy :)
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/list"))
+                .andExpect(model().attribute("products", hasSize(2)));
+    }
+
+    @Test
+    public void getProduct() throws Exception {
+        Long id = 1L;
+
+        //Tell Mockito stub to return new product for ID 1
+        when(productService.getById(id)).thenReturn(new Product());
+
+        mockMvc.perform(get("/product/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/show"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+    }
+
+    @Test
+    public void editProduct() throws Exception {
+        Long id = 1L;
+
+        //Tell Mockito stub to return new product for ID 1
+        when(productService.getById(id)).thenReturn(new Product());
+
+        mockMvc.perform(get("/product/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/form"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+
+    }
+
+    @Test
+    public void newProduct() throws Exception {
+        //should not call service
+        verifyZeroInteractions(productService);
+
+        mockMvc.perform(get("/product/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("product/form"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)));
+    }
+
+    @Test
+    public void saveOrUpdateProduct() throws Exception {
+        Long id = 1L;
+
+        String description = "Test Description";
+        BigDecimal price = new BigDecimal("12.00");
+        String imageUrl = "gladysz.me";
+
+        Product returnProduct = new Product();
+        returnProduct.setId(id);
+        returnProduct.setDescription(description);
+        returnProduct.setPrice(price);
+        returnProduct.setImageUrl(imageUrl);
+
+        when(productService.saveOrUpdate(Matchers.any(Product.class))).thenReturn(returnProduct);
+
+        mockMvc.perform(post("/product")
+                .param("id", id.toString())
+                .param("description", description)
+                .param("price", price.toString())
+                .param("imageUrl", imageUrl))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/product/show/1"))
+                .andExpect(model().attribute("product", instanceOf(Product.class)))
+                .andExpect(model().attribute("product", hasProperty("id", is(id))))
+                .andExpect(model().attribute("product", hasProperty("description", is(description))))
+                .andExpect(model().attribute("product", hasProperty("price", is(price))))
+                .andExpect(model().attribute("product", hasProperty("imageUrl", is(imageUrl))));
+
+        //verify properties of bound object
+        ArgumentCaptor<Product> boundProduct = ArgumentCaptor.forClass(Product.class);
+        verify(productService).saveOrUpdate(boundProduct.capture());
+
+        assertEquals(id, boundProduct.getValue().getId());
+        assertEquals(description, boundProduct.getValue().getDescription());
+        assertEquals(price, boundProduct.getValue().getPrice());
+        assertEquals(imageUrl, boundProduct.getValue().getImageUrl());
+    }
+
+    @Test
+    public void deleteProduct() throws Exception {
+        Long id = 1L;
+
+        mockMvc.perform(get("/product/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/product/list"));
+
+        verify(productService, times(1)).delete(id);
+    }
+
+}
