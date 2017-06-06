35bc8fbfc95822d8a247d13ea1404e911f95ca4e
Łukasz Gładysz <dev@gladysz.me>
2017-06-06 23:48:12 +0200

    Add JPA Service for Customer

--- src/main/java/me/gladysz/service/CustomerServiceImpl.java
+++ src/main/java/me/gladysz/service/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package me.gladysz.service;
 
 import me.gladysz.model.Customer;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@Profile("map")
 public class CustomerServiceImpl extends AbstractMapService implements CustomerService {
 
     @Override
