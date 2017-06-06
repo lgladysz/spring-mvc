6e50884bc8a94b67db5b0a84dd962733a89a3ee6
Łukasz Gładysz <dev@gladysz.me>
2017-06-06 23:51:43 +0200

    Add data to Customer bootstrap

--- src/main/java/me/gladysz/bootstrap/SpringJPABootstrap.java
+++ src/main/java/me/gladysz/bootstrap/SpringJPABootstrap.java
@@ -1,6 +1,8 @@
 package me.gladysz.bootstrap;
 
+import me.gladysz.model.Customer;
 import me.gladysz.model.Product;
+import me.gladysz.service.CustomerService;
 import me.gladysz.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -15,14 +17,58 @@ public class SpringJPABootstrap implements ApplicationListener<ContextRefreshedE
 
     private final ProductService productService;
 
+    private final CustomerService customerService;
+
     @Autowired
-    public SpringJPABootstrap(ProductService productService) {
+    public SpringJPABootstrap(ProductService productService, CustomerService customerService) {
         this.productService = productService;
+        this.customerService = customerService;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         loadProducts();
+        loadCustomers();
+    }
+
+    private void loadCustomers() {
+
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        customer1.setFirstName("John");
+        customer1.setLastName("Wick");
+        customer1.setEmail("email@email.com");
+        customer1.setPhoneNumber("555555555");
+        customer1.setAddressLineOne("Some Street 1");
+        customer1.setAddressLineTwo("Seccond street");
+        customer1.setZipCode("55-555");
+        customer1.setCity("NYC");
+        customerService.saveOrUpdate(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2L);
+        customer2.setFirstName("Mark");
+        customer2.setLastName("Twain");
+        customer2.setEmail("emailemail@email.com");
+        customer2.setPhoneNumber("555666333");
+        customer2.setAddressLineOne("Some Street 4");
+        customer2.setAddressLineTwo("Street");
+        customer2.setZipCode("55-598");
+        customer2.setCity("Warsaw");
+        customerService.saveOrUpdate(customer2);
+
+
+        Customer customer3 = new Customer();
+        customer3.setId(3L);
+        customer3.setFirstName("Bob");
+        customer3.setLastName("Moon");
+        customer3.setEmail("email22@email.com");
+        customer3.setPhoneNumber("567891456");
+        customer3.setAddressLineOne("Street 1");
+        customer3.setAddressLineTwo("Seccond");
+        customer3.setZipCode("58-955");
+        customer3.setCity("Cracow");
+        customerService.saveOrUpdate(customer3);
     }
 
     private void loadProducts() {
