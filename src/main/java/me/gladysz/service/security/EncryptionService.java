08c6626d43b49b1cf2ad67f9e3547d77fc0294c5
Łukasz Gładysz <dev@gladysz.me>
2017-06-16 22:08:04 +0200

    Cosmetics

--- src/main/java/me/gladysz/service/security/EncryptionService.java
+++ src/main/java/me/gladysz/service/security/EncryptionService.java
@@ -2,5 +2,6 @@ package me.gladysz.service.security;
 
 public interface EncryptionService {
     String encryptString(String input);
+
     boolean checkPassword(String plainPassword, String encryptedPassword);
 }
