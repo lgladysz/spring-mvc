6aae520028361ecffa8f9f1504ba585dcfd82867
Łukasz Gładysz <dev@gladysz.me>
2017-06-16 14:31:26 +0200

    Add User service and its implementation

---
+++ src/main/java/me/gladysz/service/jpaservice/UserServiceJpaDaoImpl.java
@@ -0,0 +1,61 @@
+package me.gladysz.service.jpaservice;
+
+import me.gladysz.model.User;
+import me.gladysz.service.UserService;
+import me.gladysz.service.security.EncryptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Service
+@Profile("jpadao")
+public class UserServiceJpaDaoImpl extends AbstractJpaDaoService implements UserService {
+
+    private EncryptionService encryptionService;
+
+    @Autowired
+    public UserServiceJpaDaoImpl(EncryptionService encryptionService) {
+        this.encryptionService = encryptionService;
+    }
+
+    @Override
+    public List<User> listAll() {
+        EntityManager em = emf.createEntityManager();
+
+        return em.createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public User getById(Long id) {
+        EntityManager em = emf.createEntityManager();
+
+        return em.find(User.class, id);
+    }
+
+    @Override
+    public User saveOrUpdate(User domainObject) {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        if (domainObject.getPassword() != null) {
+            domainObject.setEncryptedPassword(encryptionService.encryptString(domainObject.getPassword()));
+        }
+
+        User savedUser = em.merge(domainObject);
+        em.getTransaction().commit();
+
+        return savedUser;
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.remove(em.find(User.class, id));
+        em.getTransaction().commit();
+    }
+}
