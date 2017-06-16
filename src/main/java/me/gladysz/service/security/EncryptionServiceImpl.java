b73ee3fdf5c0fea65c13ad1a3e17904b63534e82
Łukasz Gładysz <dev@gladysz.me>
2017-06-16 22:06:07 +0200

    Make property field final

--- src/main/java/me/gladysz/service/security/EncryptionServiceImpl.java
+++ src/main/java/me/gladysz/service/security/EncryptionServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EncryptionServiceImpl implements EncryptionService {
 
-    private StrongPasswordEncryptor strongEncryptor;
+    private final StrongPasswordEncryptor strongEncryptor;
 
     @Autowired
     public EncryptionServiceImpl(StrongPasswordEncryptor strongEncryptor) {
