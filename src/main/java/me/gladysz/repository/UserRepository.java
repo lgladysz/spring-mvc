0022577055f347aa412bd0cbd0b9b69b6321813b
Łukasz Gładysz <dev@gladysz.me>
2017-07-07 14:04:04 +0200

    Add User Repository and Repository Service implementation

---
+++ src/main/java/me/gladysz/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.gladysz.repository;
+
+import me.gladysz.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
