a68e18fa3494727d5521b796eca42e0c7c0caf12
Łukasz Gładysz <dev@gladysz.me>
2017-07-07 14:03:19 +0200

    Add Order Repository and Repository Service implementation

---
+++ src/main/java/me/gladysz/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package me.gladysz.repository;
+
+import me.gladysz.model.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
