d734119f67d871814719c19389e0663153d7ff1d
Łukasz Gładysz <dev@gladysz.me>
2017-07-11 18:49:19 +0200

    Change repeatable strings to constant attributes and change edit to use customer form

--- src/main/java/me/gladysz/controller/CustomerController.java
+++ src/main/java/me/gladysz/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package me.gladysz.controller;
 
+import me.gladysz.commands.CustomerForm;
 import me.gladysz.model.Customer;
 import me.gladysz.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CustomerController {
 
+    private static final String ATRIBUTE_1 = "customers";
+    private static final String ATRIBUTE_2 = "customer";
     private final CustomerService customerService;
 
     @Autowired
@@ -22,32 +25,46 @@ public class CustomerController {
 
     @RequestMapping({"/list", "/"})
     public String listCustomers(Model model) {
-        model.addAttribute("customers", customerService.listAll());
+        model.addAttribute(ATRIBUTE_1, customerService.listAll());
         return "customer/list";
     }
 
     @RequestMapping("/show/{id}")
     public String getCustomer(@PathVariable Long id, Model model) {
-        model.addAttribute("customer", customerService.getById(id));
+        model.addAttribute(ATRIBUTE_2, customerService.getById(id));
         return "customer/show";
     }
 
     @RequestMapping("/edit/{id}")
     public String editCustomer(@PathVariable Long id, Model model) {
-        model.addAttribute("customer", customerService.getById(id));
+        Customer customer = customerService.getById(id);
+
+        CustomerForm customerForm = new CustomerForm();
+
+        customerForm.setCustomerId(customer.getId());
+        customerForm.setCustomerVersion(customer.getVersion());
+        customerForm.setFirstName(customer.getFirstName());
+        customerForm.setLastName(customer.getLastName());
+        customerForm.setEmail(customer.getEmail());
+        customerForm.setPhoneNumber(customer.getPhoneNumber());
+        customerForm.setUserId(customer.getUser().getId());
+        customerForm.setUserVersion(customer.getUser().getVersion());
+        customerForm.setUsername(customer.getUser().getUsername());
+
+        model.addAttribute(ATRIBUTE_2, customerForm);
         return "customer/form";
     }
 
     @RequestMapping("/new")
     public String newCustomer(Model model) {
-        model.addAttribute("customer", new Customer());
+        model.addAttribute(ATRIBUTE_2, new CustomerForm());
         return "customer/form";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String saveOrUpdateCustomer(Customer domainObject) {
-        Customer savedDomainObject = customerService.saveOrUpdate(domainObject);
-        return "redirect:/customer/show/" + savedDomainObject.getId();
+    public String saveOrUpdateCustomer(CustomerForm customerForm) {
+        Customer newCustomer = customerService.saveOrUpdateCustomerForm(customerForm);
+        return "redirect:/customer/show/" + newCustomer.getId();
     }
 
     @RequestMapping("delete/{id}")
